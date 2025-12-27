d24b04433b83b6e82c5458eabe7c8a50db0ee722
John Doe <johndoe@example.com>
2025-12-27 14:41:04 +0500

    bus and sort

---
+++ Main.java
@@ -0,0 +1,29 @@
+public class Main{
+    public static void main(String[] args) {
+        List<Bus> buses = new ArrayList<>();
+
+        buses.add(new Bus.Builder()
+                .setNumber(12)
+                .setModel("MAN")
+                .setMileage(120000)
+                .build());
+
+        buses.add(new Bus.Builder()
+                .setNumber(5)
+                .setModel("Volvo")
+                .setMileage(90000)
+                .build());
+
+        buses.add(new Bus.Builder()
+                .setNumber(20)
+                .setModel("Ikarus")
+                .setMileage(150000)
+                .build());
+
+        BubbleSort.sortByMileage(buses);
+
+        for (Bus bus : buses) {
+            System.out.println(bus);
+        }
+    }
+}
