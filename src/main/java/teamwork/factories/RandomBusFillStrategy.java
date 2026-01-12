84f301a58bc6aaf145679b02281cb0e2518dce6a
Dmitrii Popov <hiderline@yandex.ru>
2026-01-12 22:58:00 +0700

    fix stream in RandomBusFillStrategy

--- src/main/java/teamwork/factories/RandomBusFillStrategy.java
+++ src/main/java/teamwork/factories/RandomBusFillStrategy.java
@@ -5,9 +5,13 @@ import teamwork.models.Bus;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.stream.Stream;
+
+import static java.util.stream.StreamSupport.stream;
 
 public class RandomBusFillStrategy implements BusFillStrategy {
     private static final int BASE_NUMBER = 100;
+    private static final int RANDOM_NUMBER_LIMIT = 899;
     private static final String BASE_NAME = "RandomModel";
     private static final int BASE_MODEL = 10;
     private static final int BASE_ODOMETER = 1_000_000;
@@ -15,25 +19,14 @@ public class RandomBusFillStrategy implements BusFillStrategy {
     @Override
     public List<Bus> fillBuses(int size) {
         Random random = new Random();
-        List<Bus> buses = new ArrayList<>();
-        List<Integer> busesNumber = new ArrayList<>();
-//        for (int i = 0; i < size; i++) {
-//            Bus bus = new Bus.Builder()
-//                    .setNumber(BASE_NUMBER + i)
-//                    .setModel(BASE_NAME + random.nextInt(BASE_MODEL))
-//                    .setOdometer(random.nextInt(BASE_ODOMETER))
-//                    .build();
-//            buses.add(bus);
-//        }
+        List<Bus> buses;
 
-        for (int i = 0; i < size; i++) {
-            busesNumber.add(i);
-        }
-        buses = busesNumber.stream()
-                .map(bus -> new Bus.Builder()
-                        .setNumber(BASE_NUMBER + bus)
+        buses = Stream.generate(() -> new Bus.Builder()
+                        .setNumber(BASE_NUMBER + random.nextInt(RANDOM_NUMBER_LIMIT))
                         .setModel(BASE_NAME + random.nextInt(BASE_MODEL))
-                        .setOdometer(random.nextInt(BASE_ODOMETER)).build())
+                        .setOdometer(random.nextInt(BASE_ODOMETER))
+                        .build())
+                .limit(size)
                 .toList();
 
         return buses;
